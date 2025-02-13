2444c0633cae908565b61861f32914df5826b7a8
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-02-13 17:16:40 -0600

    feat: add tests for capitalize and reverseString
    Implemented and tested capitalize() and reverseString().
    Work on calculator() is still in progress.

---
+++ tests.js
@@ -0,0 +1,21 @@
+export const capitalize = (str) => {
+  //dog => Dog
+  //dogs rule the world => Dogs rules the world
+  let split = [...str];
+  split[0] = split[0].toUpperCase();
+  let result = split.join("");
+  return result;
+};
+export const reverseString = (str) => {
+  //bunny => ynnub
+  //diabolical => lacilobaid
+  let arr = [...str];
+  let reversedArr = [];
+  for (let i = 0; i < str.length; i++) {
+    reversedArr.push(arr[arr.length - 1]);
+    arr.pop(arr[arr.length - 1]);
+  }
+  let result = reversedArr.join("");
+  return result;
+};
+export const calculator = {};
---
+++ tests.test.js
@@ -0,0 +1,34 @@
+// Capitalize tests
+import { capitalize } from "./tests";
+test("Capitalized first letter", () => {
+  expect(capitalize("dog")).toEqual("Dog");
+});
+test("Capitalize first letter if multiple words exist", () => {
+  expect(capitalize("dogs rule the world")).toEqual("Dogs rule the world");
+});
+
+// reverseString Tests
+import { reverseString } from "./tests";
+test("reversed diabolical", () => {
+  expect(reverseString("diabolical")).toEqual("lacilobaid");
+});
+test("reversed bunny", () => {
+  expect(reverseString("bunny")).toEqual("ynnub");
+});
+// Calculator Tests
+import { calculator } from "./tests";
+test("addition", () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
+test("subtraction", () => {
+  expect(calculator.subtract(2, 3)).toBe(-1);
+});
+test("divide", () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+test("multiply", () => {
+  expect(calculator.add(2, 3)).toBe(6);
+});
+// caesarCipher Tests
+
+// analyzeArray TestsP
